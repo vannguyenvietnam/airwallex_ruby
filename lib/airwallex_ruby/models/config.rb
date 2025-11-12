85644c2bd662ea0adb96b79b98778c8167f810a9
Van Nguyen <bichvannguyenvnn@gmail.com>
2025-11-12 16:15:33 +1100

    Update code

--- lib/airwallex_ruby/models/config.rb
+++ lib/airwallex_ruby/models/config.rb
@@ -37,6 +37,74 @@ module AirwallexRuby
 
         response_body['items'].map { |item| new(data: item.deep_symbolize_keys) }
       end
+
+      # Fetches available bank names using the API.
+      #
+      # @param [Hash] options
+      # @options [String] :payment_method_type - The payment method type to find the available banks. For other payment methods that don't require bank_name, an empty list will be returned.
+      # @options [String] :country_code - For payment method type like online_banking and bank_transfer, the available bank list differs by different countries and country_code is needed to get the bank list.
+      # @options [Integer] :page_num - Page number starting from 0
+      # @options [Integer] :page_size - Number of payment method types to be listed per page. Default value is 100. Maximum is 1000. The value greater than the maximum will be capped to the maximum.
+      # @return [AirwallexRuby::AccountCapability]
+      def self.banks options = {}
+        request_url = URI.parse(AirwallexRuby.api_url).tap do |uri|
+          uri.path += "#{ENDPOINT}/banks"
+        end
+
+        request_url.query = init_params_for_request(options)
+        response = get(request_url, {}, options)
+        response_body = JSON.parse(response.body)
+        return [] if response_body['items'].nil?
+
+        response_body['items'].map { |item| new(data: item.deep_symbolize_keys) }
+      end
+
+      # Fetches current apple pay registered domains using the API.
+      #
+      # @return [AirwallexRuby::Config]
+      def self.applepay_registered_domains options = {}
+        request_url = URI.parse(AirwallexRuby.api_url).tap { |uri| uri.path += "#{ENDPOINT}/applepay/registered_domains" }
+        response = get(request_url, {}, options)
+        response_body = JSON.parse(response.body).deep_symbolize_keys
+        new(data: response_body)
+      end
+
+      # Fetches current convertible shopper currencies using the API.
+      #
+      # @return [AirwallexRuby::Config]
+      def self.convertible_shopper_currencies options = {}
+        request_url = URI.parse(AirwallexRuby.api_url).tap { |uri| uri.path += "#{ENDPOINT}/convertible_shopper_currencies" }
+        response = get(request_url, {}, options)
+        response_body = JSON.parse(response.body).deep_symbolize_keys
+        new(data: response_body)
+      end
+
+      # Add apple pay registered domains using the API.
+      #
+      # @return [AirwallexRuby::Config]
+      def self.add_applepay_registered_domains domain_data, options = {}
+        request_url = URI.parse(AirwallexRuby.api_url).tap do |uri| 
+          uri.path += "#{ENDPOINT}/applepay/registered_domains/add_items" 
+        end
+
+        response = post(request_url, domain_data, options)
+        response_body = JSON.parse(response.body).deep_symbolize_keys
+        new(data: response_body)
+      end
+
+      # Remove apple pay registered domains using the API.
+      #
+      # @return [AirwallexRuby::Config]
+      def self.remove_applepay_registered_domains domain_data, options = {}
+        request_url = URI.parse(AirwallexRuby.api_url).tap do |uri| 
+          uri.path += "#{ENDPOINT}/applepay/registered_domains/remove_items" 
+        end
+
+        response = post(request_url, domain_data, options)
+        response_body = JSON.parse(response.body).deep_symbolize_keys
+        new(data: response_body)
+      end
+      # End of class Config
     end
   end
 end
